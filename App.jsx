666c3123d73b6c9e038f36ad94116b60d4d9dc55
anttmin <anttmin@gmail.com>
2024-01-08 20:48:54 +0630

    commit

--- App.jsx
+++ App.jsx
@@ -1,17 +1,31 @@
-import Main from "./layout/Main"
-import ItemContextProvider from "./Store/ItemContent"
+import { useEffect, useState } from "react";
+import ProductCard from "./components/ProductCard";
 
-const App = () => {
-  return (
- 
-   <>
-    <ItemContextProvider>
-    <Main />
-    </ItemContextProvider>
-   </>
 
-   
-  )
+
+function App(){
+ const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+       getProducts(); 
+    },[]);
+
+    const getProducts = async () =>{
+        const response = await fetch ('https://fakestoreapi.com/products');
+        const products = await response.json();
+
+        setProducts(products)
+
+    }
+    
+    return(
+        <>
+          {
+            products.map((product)=>(
+               <ProductCard key={product.id} product={product} />
+            ))
+          }
+        </>
+    )
 }
-
 export default App
